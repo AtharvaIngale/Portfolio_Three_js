5e7fd0921eb349895930aa76a0f7d2161a994b16
Atharva Ingale <akingale00@gmail.com>
2024-10-16 19:29:46 +0530

    Navbar added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './sections/navbar'
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
