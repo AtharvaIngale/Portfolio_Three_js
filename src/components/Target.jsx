6ef25a7d47c62b2826b9a0460798779cc4fc9648
Atharva Ingale <akingale00@gmail.com>
2024-11-14 19:30:31 +0530

    Hero Section Complete

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -1,30 +1,29 @@
-import { useGLTF } from '@react-three/drei'
-import React, { useRef } from 'react'
-import gsap from 'gsap';
+
+import { useGLTF } from '@react-three/drei';
+import { useRef } from 'react';
 import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Target = (props) => {
+  const targetRef = useRef();
+  const { scene } = useGLTF(
+    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
+  );
 
-    const targetRef = useRef();
-
-    const { scene } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf');
-
-    useGSAP (() => {
-        gsap.to(targetRef.current.position, {
-            y: targetRef.current.position.y + 1,
-            duration: 1,
-            repeat: -1,
-            yoyo: true
-        })
-    })
+  useGSAP(() => {
+    gsap.to(targetRef.current.position, {
+      y: targetRef.current.position.y + 0.5,
+      duration: 1.5,
+      repeat: -1,
+      yoyo: true,
+    });
+  });
 
   return (
-    <mesh {...props} ref={targetRef} rotation={[0, Math.PI/5]}>
-
-        <primitive object={scene} />
-
+    <mesh {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
+      <primitive object={scene} />
     </mesh>
-  )
-}
+  );
+};
 
-export default Target
+export default Target;
